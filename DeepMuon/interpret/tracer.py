68e9f8d7876a0527ada0b662eb7386c0d0e5c13b
airscker <airscker@gmail.com>
2023-02-19 14:07:13 +0800

    neuron tracer complete

--- DeepMuon/interpret/tracer.py
+++ DeepMuon/interpret/tracer.py
@@ -2,50 +2,81 @@
 Author: airscker
 Date: 2023-02-15 20:13:01
 LastEditors: airscker
-LastEditTime: 2023-02-19 13:41:50
+LastEditTime: 2023-02-19 14:06:35
 Description: NULL
 
 Copyright (C) 2023 by Airscker(Yufeng), All Rights Reserved.
 '''
-import torch
 from torch import nn
-from typing import Any
-from DeepMuon.models import *
 
 class Neuron_Flow:
     '''
     ## Trace the data flow of model's neurons
 
     ### Args:
-        - model: the model to be traced
-        - input_data: the input data of the model
-        - mode: `eval`/`train`, whether to cancel the gradient refreshing and freezing normalization layer(eg. BatchNorm)
+        - model: the model to be traced, please put it on CPU, the tracing mechanism will be done under the evaluation mode
+        - input_data: the input data of the model, please put it on CPU
 
-    ### Properties:
-        - hooks: dict(module_id = forward_hook), the hooks built for every neuron to trace the data flow.
+    ### Some Properties:
+        - hooks: `dict(module_id = forward_hook)`
+            the hooks built for every neuron to trace the data flow.
             hooks will be created once you call `trace()` method and after the inference work, hooks will be removed
-        - neuron_info: dict(module_id = [neuron_name, module_name, input_data, output_data]), to enable this property,
+        - neuron_info: `dict(module_id = [neuron_name, module_name, input_data, output_data])`
+            the property records the most important info which we want, to enable this property,
             you need to call `trace()` method to record the input/output data of every neuron
+
+    ### Tips:
+        - Once you create the `Neuron_Flow` object, the tracing will be done by using data and model given, the initialization pipeline is listed as following:
+            - Get model and input data
+            - Run `trace()` to get information of neurons
+        - If you want to test another input data, please call `trace()` again and pass your input data to it
+        - You can get every neuron's input data and output data by refering property `neuron_info`
+    
+    ### Examples:
+
+    >>> NeuronF=Neuron_Flow(model=screening_model(),input_data=torch.rand(1,3,13,100,100))
+    >>> print(NeuronF)
+    >>> Model Architecture: screening_model(
+            (vst): SwinTransformer3D(
+                (patch_embed): PatchEmbed3D(
+                (proj): Conv3d(3, 128, kernel_size=(2, 4, 4), stride=(2, 4, 4))
+                (norm): LayerNorm((128,), eps=1e-05, elementwise_affine=True)
+            ...
+            Module ID: 2745179684480, Neuron Name: linear, Module Name: Linear
+                input[0]: torch.Size([1, 1024])
+                output[0]: torch.Size([11])
+    >>> # Trace another flow
+    >>> NeuronF.trace(torch.rand(2,3,13,120,120))
+    >>> print(NeuronF)
+    >>> Model Architecture: screening_model(
+            (vst): SwinTransformer3D(
+                (patch_embed): PatchEmbed3D(
+                (proj): Conv3d(3, 128, kernel_size=(2, 4, 4), stride=(2, 4, 4))
+                (norm): LayerNorm((128,), eps=1e-05, elementwise_affine=True)
+            ...
+            Module ID: 2745179684480, Neuron Name: linear, Module Name: Linear
+                input[0]: torch.Size([2, 1024])
+                output[0]: torch.Size([11])
+                output[1]: torch.Size([11])
     '''
-    def __init__(self,model:nn.Module,input_data,mode='eval'):
+    def __init__(self,model:nn.Module,input_data):
         self.model=model
-        if mode=='eval':
-            self.model.eval()
-        elif mode=='train':
-            self.model.train()
+        self.model.eval()
         self.input=input_data
         self.hooks={}
         self.neuron_info={}
         self.trace()
     def __tracer_hook(self,module,input,output):
+        '''Hook built for tracing data flow'''
         self.neuron_info[id(module)]+=[input,output]
     def __clean_hooks(self):
         '''Clean hooks of every neuron''' 
         for module_id in self.hooks.keys():
             self.hooks[module_id].remove()
     def __apply_hooks(self):
+        '''Apply tracing hook for every neuron recursively'''
         model_name=self.model.__class__.__name__
-        stack: list[tuple[str, nn.Module, int, None]] = [(model_name, self.model, 0)]
+        stack: list[tuple[str, nn.Module, int]] = [(model_name, self.model, 0)]
         while stack:
             var_name, module, curr_depth = stack.pop()
             module_id = id(module)
