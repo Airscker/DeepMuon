b56793867a0ed449a0db58b8c7ee4e0cf78a772c
airscker <airscker@gmail.com>
2023-02-25 14:39:08 +0800

    bug fix

--- assets/js/load_html.js
+++
@@ -1,4 +0,0 @@
-$(function () {
-  $("#navigation").load("https://airscker.github.io/DeepMuon/navigation.html");
-  $("#footer").load("https://airscker.github.io/DeepMuon/footer.html");
-});
---
+++ assets/js/navigation.js
@@ -0,0 +1,4 @@
+$(function () {
+  $("#page_nav").load("https://airscker.github.io/DeepMuon/navigation.html");
+  $("#page_foot").load("https://airscker.github.io/DeepMuon/page_foot.html");
+});
