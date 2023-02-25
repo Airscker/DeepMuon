c83d32521e5c9afe2418aa9f99d580384de00440
airscker <airscker@gmail.com>
2023-02-25 14:22:56 +0800

    improvement

--- assets/js/load_html.js
+++ assets/js/load_html.js
@@ -1,4 +1,4 @@
-$(function() {
-    $("#navigation").load("https://airscker.github.io/DeepMuon/navigation.html");
-    $("footer").load("https://airscker.github.io/DeepMuon/footer.html");
+$(function () {
+  $("#navigation").load("https://airscker.github.io/DeepMuon/navigation.html");
+  $("#footer").load("https://airscker.github.io/DeepMuon/footer.html");
 });
