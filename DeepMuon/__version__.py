4119de7f4dcaac4684f8644776d0bf29cd97f798
airscker <airscker@gmail.com>
2023-03-01 13:35:22 +0800

    1.23.31

--- DeepMuon/__version__.py
+++ DeepMuon/__version__.py
@@ -2,9 +2,9 @@
 Author: airscker
 Date: 2022-09-21 18:46:04
 LastEditors: airscker
-LastEditTime: 2023-02-23 12:34:19
+LastEditTime: 2023-03-01 13:35:06
 Description: Version file of DeepMuon
 
 Copyright (C) 2023 by Airscker(Yufeng), All Rights Reserved. 
 '''
-__version__ = '1.23.23'
+__version__ = '1.23.31'
